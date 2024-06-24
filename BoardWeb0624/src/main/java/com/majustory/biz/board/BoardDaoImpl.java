cb7820e05772df2601c6f5879a47140b6b03331a
minju0809 <loverminju88@naver.com>
2024-06-24 17:51:28 +0900

    240624 어노테이션 AOP, 스프링 JDBC 사용(오라클 연동) insert

--- BoardWeb0624/src/main/java/com/majustory/biz/board/BoardDaoImpl.java
+++ BoardWeb0624/src/main/java/com/majustory/biz/board/BoardDaoImpl.java
@@ -4,32 +4,34 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import com.majustory.biz.comm.JDBCUtil;
 
 @Repository
-public class BoardDaoImpl implements BoardDao {
+public class BoardDaoImpl extends JdbcDaoSupport implements BoardDao {
 	
 	private Connection conn = null;
 	private PreparedStatement pstmt = null;	
 	private ResultSet rs =null;
+	
+	@Autowired
+	public void setSuperDataSource(DataSource dataSource) {
+		super.setDataSource(dataSource);
+	}
+
+	String INSERTSQL = "insert  into board (seq, title, writer, content) "
+			+ " values(idx.nextval, ?, ?, ?) "; 
+	
 	@Override
 	public void insert(BoardVO vo) {
 		System.out.println("==>  BoardDaoImpl insert(BoardVO vo) ");
-		conn  = JDBCUtil.getConnection();
-		String SQL = "insert  into board (seq, title, writer, content) "
-				+ " values(idx.nextval, ?, ?, ?) "; 
-		try {
-		   pstmt =	conn.prepareStatement(SQL);
-		   pstmt.setString(1, vo.getTitle());
-		   pstmt.setString(2, vo.getWriter());
-		   pstmt.setString(3, vo.getContent());
-		   pstmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 		
+		getJdbcTemplate().update(INSERTSQL, vo.getTitle(), vo.getWriter(), vo.getContent());
 	}
 
 	@Override
--- BoardWeb0624/src/main/java/com/majustory/biz/comm/LogAdvice.java
+++ BoardWeb0624/src/main/java/com/majustory/biz/comm/LogAdvice.java
@@ -1,14 +1,29 @@
 package com.majustory.biz.comm;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+@Service
+@Aspect
 public class LogAdvice {
 	LogAdvice() {
 		System.out.println("LogAdvice 생성자 확인");
 	}
 	
+	@Pointcut("execution(* com.majustory.biz.board..*ServiceImpl.*(..))")
+	public void allPointcut() {
+		
+	}
+	
+	@Before("allPointcut()")
 	public void beforeLog() {
 		System.out.println("beforeLog");
 	}
 	
+	@After("allPointcut()")
 	public void afterLog() {
 		System.out.println("afterLog");
 	}
